f7b149832108e2258ae7b97ad7324788ba49eefe
Ziyang Tan <arcopus07@gmail.com>
2026-09-11 19:49:01 +0800

    自制双层Kelvin–Voigt击锤，加入bbpl的loss与frac滤波器，目前效果不错
    # 260911
    ## 我自己建立了一个hammer模型，它很可能并不对应什么特别的方式，但我打算试一试
    ## 先把自由度做全，然后启发式学习找高拟合解簇，从里面选择最物理正确的

--- Cli/bBpiano Lite/core/bbpl/piano/fractional_filter.hpp
+++ Cli/bBpiano Lite/core/bbpl/piano/fractional_filter.hpp
@@ -10,4 +10,30 @@
 
 #include <iostream>
 
+class FractionalFilter {
+    float a1 = 0.0;
+    
+    float x1 = 0.0;
+    float y1 = 0.0;
+    
+public:
+    
+    FractionalFilter(double frac) {
+        a1 = (1 - frac) / (1 + frac);
+    }
+    
+    inline void process(float &x) {
+         // y = a1 * x + x1 - a1 * y1;
+         float y = static_cast<float>(a1 * x)
+             + static_cast<float>(x1)
+             - static_cast<float>(a1 * y1);
+         x1 = x;
+         y1 = y;
+         x = y;
+     }
+};
+
+
+
+
 #endif /* fractional_filter_hpp */
---
+++ Cli/bBpiano Lite/core/bbpl/piano/src/loss_filter.cpp
@@ -0,0 +1,8 @@
+//
+//  loss_filter.cpp
+//  bbpl
+//
+//  Created by opus arc on 2026/9/11.
+//
+
+#include "loss_filter.hpp"
